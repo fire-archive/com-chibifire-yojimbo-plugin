4bc8c32c210e7c83bb1b0ffbe48069a3e83e875b
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2018-01-23 16:09:38 -0800

    Cleanup.

--- sample/main.gd
+++ sample/main.gd
@@ -5,8 +5,10 @@ var yojimbo
 
 func _ready():
 	yojimbo = native_yojimbo.new()
+#	yojimbo.create_server(4000, 2, 0, 0)
+#	yojimbo.poll()
 	yojimbo.create_client("127.0.0.1", 4000, 0, 0)
-	get_tree().set_network_peer(yojimbo)
+#	get_tree().set_network_peer(yojimbo)
 
 func _on_Button_pressed():
 	pass
