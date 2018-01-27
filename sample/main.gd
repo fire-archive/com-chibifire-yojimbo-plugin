2ec0897492493dbe82c150d58ed667df88c15dd9
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2018-01-27 10:24:19 -0800

    Set logging to debug.

--- sample/main.gd
+++ sample/main.gd
@@ -6,12 +6,12 @@ var yojimbo
 func _ready():
 	yojimbo = native_yojimbo.new()
 	yojimbo.create_server(40000, 2, 0, 0)
-
+	yojimbo.create_client("127.0.0.1", 40000, 0, 0)
+	yojimbo.set_log_level(3)
 #	get_tree().set_network_peer(yojimbo)
 
 func _physics_process(delta):
 	yojimbo.poll()
 
 func _on_Button_pressed():
-	yojimbo.create_client("127.0.0.1", 40000, 0, 0)
-	yojimbo.set_log_level(2)
+	pass
